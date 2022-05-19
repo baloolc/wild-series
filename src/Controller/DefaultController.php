03b65a32a4b5f1e9c4d421cefd2c87bdd153fa85
baloolc <viann45@hotmail.fr>
2022-05-19 16:15:56 +0200

    symfony 03

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ProgramController extends AbstractController
+
+{
+    #[Route('/', name: 'app_index')]
+
+    public function index(): Response
+
+    {
+
+        return $this->render('index.html.twig');
+
+    }
+
+}
