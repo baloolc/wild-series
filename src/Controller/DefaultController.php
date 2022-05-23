bc18983e25fffde17c393e380b6c58c1c14a9cb5
baloolc <viann45@hotmail.fr>
2022-05-23 17:11:25 +0200

    create show controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,23 +4,16 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\Routing\Annotation\Route;
-
 use Symfony\Component\HttpFoundation\Response;
 
 
-class ProgramController extends AbstractController
-
+class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-
     public function index(): Response
-
     {
-
         return $this->render('index.html.twig');
-
     }
 
 }
