0fffb0a877477fb741297a9068ce738b28927fd5
baloolc <viann45@hotmail.fr>
2022-05-26 21:16:03 +0200

    form category and form program

---
+++ .history/src/Controller/CategoryController_20220526195541.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
+
+class CategoryController extends AbstractController
+{
+    #[Route('/category/', name: 'category_index')]
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+        return $this->render('Category/index.html.twig', ['categories' => $categories]);
+    }
+
+    #[Route('/category/new', name: 'category_new')]
+    public function new(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+
+        }
+
+        return $this->renderForm('Category/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('category/{name}', name: 'category_show')]
+    public function show(string $name, ProgramRepository $programRepository, CategoryRepository $categoryRepository ): Response
+    {
+        $category = $categoryRepository->findOneByName(['name' => $name]);
+        if(!$category)
+        {
+            throw $this->createNotFoundException(
+                'No category with name : '. $category .' found in category\'s table.'
+            );
+        }
+        
+        $programs = $programRepository->findBy(['category' => $category]);
+
+            if (!$programs) {
+                throw $this->createNotFoundException(
+                   'No program with category : '. $programs .' found in program\'s table.'
+                );
+            }      
+    
+        return $this->render('Category/show.html.twig', [
+            'programs' => $programs, 'category' => $category,
+        ]);
+    }
+}
---
+++ .history/src/Form/ProgramType_20220526203613.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, ['placeholder' => 'Titre'])
+            ->add('synopsis', TextType::class, ['placeholder' => 'Synopsis'])
+            ->add('poster', TextType::class, [
+                'required' => false,])
+            ->add('country', TextType::class , ['placeholder' => 'Le pays'])
+            ->add('year', TextType::class, ['placeholder' => 'L\'année'])
+            ->add('category', TextType::class, ['placeholder' => 'Titre'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+            'data_class' => Category::class,
+        ]);
+    }
+}
