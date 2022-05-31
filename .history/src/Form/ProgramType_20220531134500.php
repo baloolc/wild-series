8ba8f9efeb8c22f71424bced7d289154820b6d10
baloolc <viann45@hotmail.fr>
2022-05-31 15:11:04 +0200

    relation actor and program crud

---
+++ .history/src/Form/ProgramType_20220531134500.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ProgramType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('synopsis', TextType::class, ['label' => 'Synopsis'])
+            ->add('poster', TextType::class, [
+                'required' => false, 'label' => 'Photo'])
+            ->add('country', TextType::class , ['label' => 'Pays'])
+            ->add('year', TextType::class, ['label' => 'Année'])
+            ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+
+                'choice_label' => 'name',
+            
+                'multiple' => true,
+            
+                'expanded' => false,
+            
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Program::class,
+        ]);
+    }
+}
