40f8159409609426ea9452ecc4fbff23babc08bd
baloolc <viann45@hotmail.fr>
2022-06-02 13:03:30 +0200

    mailer

---
+++ .history/src/Controller/EpisodeController_20220602122437.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Episode;
+use App\Service\Slugify;
+use App\Form\EpisodeType;
+use App\Repository\EpisodeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/episode')]
+class EpisodeController extends AbstractController
+{
+    #[Route('/', name: 'app_episode_index', methods: ['GET'])]
+    public function index(EpisodeRepository $episodeRepository): Response
+    {
+        return $this->render('episode/index.html.twig', [
+            'episodes' => $episodeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_episode_new', methods: ['GET', 'POST'])]
+    public function new(Request $request,MailerInterface $mailer, EpisodeRepository $episodeRepository, Slugify $slugify): Response
+    {
+        $episode = new Episode();
+        $form = $this->createForm(EpisodeType::class, $episode);
+        $form->handleRequest($request);
+        $slug = $slugify->generate($episode->getTitle());
+        $episode->setSlug($slug);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $episodeRepository->add($episode, true);
+
+            return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('episode/new.html.twig', [
+            'episode' => $episode,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_episode_show', methods: ['GET'])]
+    public function show(Episode $episode): Response
+    {
+        return $this->render('episode/show.html.twig', [
+            'episode' => $episode,
+        ]);
+    }
+
+    #[Route('/{slug}/edit', name: 'app_episode_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Episode $episode, EpisodeRepository $episodeRepository): Response
+    {
+        $form = $this->createForm(EpisodeType::class, $episode);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $episodeRepository->add($episode, true);
+
+            return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('episode/edit.html.twig', [
+            'episode' => $episode,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{slug}', name: 'app_episode_delete', methods: ['POST'])]
+    public function delete(Request $request, Episode $episode, EpisodeRepository $episodeRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$episode->getId(), $request->request->get('_token'))) {
+            $episodeRepository->remove($episode, true);
+        }
+
+        return $this->redirectToRoute('app_episode_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
